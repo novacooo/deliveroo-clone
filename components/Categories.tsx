d81d4477bfbbbf3960dfd12936e342894db28ee5
Jacek Nowak <jaceknowak226@gmail.com>
2022-08-09 21:37:51 +0200

    Add body

---
+++ components/Categories.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+const Categories = () => (
+  <View>
+    <Text>Categories</Text>
+  </View>
+);
+
+export default Categories;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { useLayoutEffect } from 'react';
 import { Image, ScrollView, Text, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline';
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -14,33 +15,41 @@ const HomeScreen = () => {
   });
 
   return (
-    <SafeAreaView className="bg-white px-4">
-      {/* Header */}
-      <View className="flex-row py-3 items-center space-x-2">
-        <Image
-          source={{
-            uri: 'https://links.papareact.com/wru',
-          }}
-          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-        />
-        <View className="flex-1">
-          <Text className="font-bold text-gray-400 text-xs">Deliver now!</Text>
-          <View className="flex-row items-center">
-            <Text className="font-bold text-lg">Current location</Text>
-            <ChevronDownIcon size={18} color="#00ccbb" />
+    <SafeAreaView className="bg-white flex-1">
+      {/* Top bar */}
+      <View className="bg-white px-4 py-3 gap-3">
+        {/* Header */}
+        <View className="flex-row items-center space-x-2">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver now!</Text>
+            <View className="flex-row items-center">
+              <Text className="font-bold text-lg">Current location</Text>
+              <ChevronDownIcon size={18} color="#00ccbb" />
+            </View>
           </View>
+          <UserIcon size={28} color="#00ccbb" />
         </View>
-        <UserIcon size={28} color="#00ccbb" />
-      </View>
-      {/* Search */}
-      <View className="flex-row items-center space-x-2 pb-2">
-        <View className="flex-row flex-1 items-center space-x-2 bg-gray-200 mr-2 p-3">
-          <SearchIcon size={20} color="gray" />
-          <TextInput placeholder="Restaurants and cuisines" keyboardType="default" className="flex-1" />
+        {/* Search */}
+        <View className="flex-row items-center space-x-2">
+          <View className="flex-row flex-1 items-center space-x-2 bg-gray-200 mr-2 p-3">
+            <SearchIcon size={20} color="gray" />
+            <TextInput placeholder="Restaurants and cuisines" keyboardType="default" className="flex-1" />
+          </View>
+          <AdjustmentsIcon size={28} color="#00ccbb" />
         </View>
-        <AdjustmentsIcon size={28} color="#00ccbb" />
       </View>
-      <ScrollView />
+      {/* Body */}
+      <ScrollView className="bg-gray-100 px-4 flex-1">
+        {/* Categories */}
+        <Categories />
+        {/* Featured rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
