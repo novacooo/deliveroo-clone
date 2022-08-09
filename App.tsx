09c0651963ff319ab6fabf3111643d26db9a4f37
Jacek Nowak <jaceknowak226@gmail.com>
2022-08-09 19:33:20 +0200

    Setup tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
 
 const App = () => (
-  <View>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
-)
+  <TailwindProvider>
+    <View>
+      <Text className="text-red-500 p-10">Open up App.tsx to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  </TailwindProvider>
+);
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  content: ['./App.{js,ts,jsx,tsx}', './screens/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+};
