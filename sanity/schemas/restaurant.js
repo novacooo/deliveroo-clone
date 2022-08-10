d7b76dc1296c1fd1f4b3fed38df0672da383d874
Jacek Nowak <jaceknowak226@gmail.com>
2022-08-10 20:25:52 +0200

    Setup schemas for sanity

---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Restaurant name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'shortDescription',
+      type: 'string',
+      title: 'Short description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image of the restaurant',
+    },
+    {
+      name: 'lat',
+      type: 'number',
+      title: 'Latitude of the restaurant',
+    },
+    {
+      name: 'long',
+      type: 'number',
+      title: 'Longitude of the restaurant',
+    },
+    {
+      name: 'address',
+      type: 'string',
+      title: 'Restaurant address',
+    },
+  ],
+};
